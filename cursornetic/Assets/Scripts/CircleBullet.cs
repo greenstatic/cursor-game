0bfefcd4397624dec17b12e9fda8ddda6e53a39e
greenstatic <gregor@krmelj.xyz>
2019-11-13 23:41:16 +0100

    player shooting mechanics

---
+++ cursornetic/Assets/Scripts/CircleBullet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleBullet : MonoBehaviour
+{
+    public float speed = 1f;
+    public Rigidbody2D rb;
+    public float lifeDuration = 1f;
+    public int damage = 100;
+
+    private float lifeTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        lifeTimer = lifeDuration;
+    }
+
+    void Update()
+    {
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0f)
+        {
+            Debug.Log("Bullet expired");
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        // Do not interact with player
+        if (hit.name == "Player")
+        {
+            return;
+        }
+
+        // If hit enemy, reduce health of enemy
+        if (hit.name == "Enemy")
+        {
+            EnemyController enemy = hit.gameObject.GetComponent<EnemyController>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+        }
+
+        //Debug.Log(hit.name);
+        Destroy(gameObject);
+    }
+}
--- cursornetic/Assets/Scripts/EnemyController.cs
+++ cursornetic/Assets/Scripts/EnemyController.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
     public ParticleSystem deathParticle;
+    public int health = 100;
 
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.CompareTag("Player")) {
             if (true) //we need a parameter that tells us if the player is dashing!!
-                Destroy();
+                Die();
         }
     }
 
-    public void Destroy() {
+    public void Die()
+    {
         Destroy(gameObject);
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+
     }
 }
