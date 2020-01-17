3d36af3324e2a64b70f7378b307003d3b226e0f8
Federico <fedeser9616@gmail.com>
2020-01-17 02:57:40 +0100

    Polishing

---
+++ cursornetic/Assets/Scripts/BulletClipManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class BulletClipManager : MonoBehaviour {
+
+    private VideoPlayer videoPlayer;
+    private double length;
+
+    public void Start() {
+        if (GlobalState.introPlayed) {
+            Destroy(gameObject);
+        } else {
+            videoPlayer = GetComponent<VideoPlayer>();
+            length = videoPlayer.clip.length;
+
+            StartCoroutine(WaitForVideo());
+        }
+    }
+
+    IEnumerator WaitForVideo() {
+        yield return new WaitForSeconds((int)length + 1);
+        GlobalState.introPlayed = true;
+        Destroy(gameObject);
+    }
+
+}
